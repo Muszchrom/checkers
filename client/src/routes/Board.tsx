1dbcc8633506e3d5b7f8ef80300f931a544b99f2
Muszchrom <altair2468@o2.pl>
2023-11-23 15:01:09 +0100

    Code refactorization + king bug fix

--- client/src/routes/Board.tsx
+++ client/src/routes/Board.tsx
@@ -2,14 +2,19 @@ import Piece from "../components/boardAssets/Piece"
 import { useState } from "react"
 
 export default function Board() {
+  // color is replaced by formula 
+  // row_id + col_id % 2 == 1 ? "White" : "Black"
+  // that's why type BoardType exist
+  // renamed dark to black because colors are easier than shades
   type Field = {
-    occupiedBy: "none" | "white" | "dark"
-    color: "bg-zinc-900" | "bg-zinc-100"
-    active?: boolean
-    isKing?: boolean
+    occupiedBy: "none" | "white" | "black"
+    active: boolean
+    isKing: boolean
   }
-  const generateFields = () => {
-    const fields: Field[][] = []
+  type BoardType = Field[][]
+
+  const generateFields = (): BoardType => {
+    const fields: BoardType = []
     let buff: Field[] = []
 
     for (let row=0; row<8; row++) {
@@ -19,8 +24,8 @@ export default function Board() {
         const isPieceAllowed = isRowEven ? col%2 : !(col%2)
         
         buff.push({
-          occupiedBy: isPieceAllowed ? (row < 3 ? "dark" : row > 4 ? "white" : "none") : "none",
-          color: isPieceAllowed ? "bg-zinc-900" : "bg-zinc-100",
+          occupiedBy: isPieceAllowed ? (row < 3 ? "black" : row > 4 ? "white" : "none") : "none",
+          active: false,
           isKing: false
         })
       }
@@ -30,11 +35,12 @@ export default function Board() {
     return fields
   }
 
-  const [fields, setFields] = useState(generateFields())
-  const [whosTurn, setWhosTurn] = useState<"white" | "dark">("white")
-  const [activePiece, setActivePiece] = useState<[number, number] | null>(null)
+  const [fields, setFields] = useState<BoardType>(generateFields())  // Board state
+  const [whosTurn, setWhosTurn] = useState<"white" | "black">("white")  // tracking turns
+  const [activePiece, setActivePiece] = useState<[number, number] | null>(null)  // [row, col] of currently active piece or null, when there's no selected piece
   
-  const clearActiveFields = (updateState?: boolean) => {
+  // set all Field.active to false, optionally update the state 
+  const clearActiveFields = (updateState?: boolean): void => {
     for (let i=0; i<8; i++) {
       for (let j=0; j<8; j++) {
         fields[i][j].active = false
@@ -46,27 +52,51 @@ export default function Board() {
     }
   }
 
-  const handlePieceClick2 = (nRow: number, nCol: number) => {
+  // basically handle piece selection and movement 
+  // movement must be checked on server, since it is easily hackable
+  const handlePieceClick2 = (nRow: number, nCol: number): void => {
     if (!activePiece) {
-      if (fields[nRow][nCol].occupiedBy !== whosTurn) return
+      // if field is not yours return void
+      if (fields[nRow][nCol].occupiedBy !== whosTurn) {
+        return 
+      }
+      // highlight available moves and save active piece in the state
       setFields(highlightAvailableMoves(nRow, nCol, fields))
       setActivePiece([nRow, nCol])
-    } if (activePiece && fields[nRow][nCol].active) {
-      const delta = Math.abs(nRow-activePiece[0])
+    } 
+    
+    if (activePiece && fields[nRow][nCol].active) {
       const calcPos = (w: number, f: number, i: number) => w-(w-f)/Math.abs((w-f))*i
-      let setTurn: "white" | "dark" = whosTurn === "white" ? "dark" : "white"
-      if (delta === 0) return clearActiveFields(true)
-      for (let i=1; i<delta; i++) {
-        if (fields[calcPos(activePiece[0], nRow, i)][calcPos(activePiece[1], nCol, i)].occupiedBy !== fields[activePiece[0]][activePiece[1]].occupiedBy) {
-          fields[calcPos(activePiece[0], nRow, i)][calcPos(activePiece[1], nCol, i)].occupiedBy = "none"
-          setTurn = whosTurn === "white" ? "white" : "dark"
+      
+      const distance = Math.abs(nRow-activePiece[0])
+      let nextTurn: "white" | "black" = whosTurn === "white" ? "black" : "white"
+      
+      // remove active styles when active piece is clicked
+      if (distance === 0) {
+        return clearActiveFields(true)
+      }
+      
+      // remove enemy pieces
+      for (let i=1; i<distance; i++) {
+        const row = calcPos(activePiece[0], nRow, i)
+        const col = calcPos(activePiece[1], nCol, i)
+        if (fields[row][col].occupiedBy !== fields[activePiece[0]][activePiece[1]].occupiedBy) {
+          fields[row][col].occupiedBy = "none"
+          nextTurn = whosTurn === "white" ? "white" : "black"
         }
       }
+
+      // swap currently active piece with destination 
       const buff = fields[nRow][nCol]
       fields[nRow][nCol] = fields[activePiece[0]][activePiece[1]]
       fields[activePiece[0]][activePiece[1]] = buff
-      if (nRow === 0 || nRow === 7) fields[nRow][nCol].isKing = true
-      setWhosTurn(setTurn)
+
+      // make piece a king, when conditions are met
+      if (nRow === 0 && fields[nRow][nCol].occupiedBy === "white" || nRow === 7 && fields[nRow][nCol].occupiedBy === "black") {
+        fields[nRow][nCol].isKing = true
+      }
+
+      setWhosTurn(nextTurn)
       setFields([...fields])
       setActivePiece(null)
       clearActiveFields(true)
@@ -86,8 +116,8 @@ export default function Board() {
     const isDirectionEnabled = [
       mainField.isKing || mainField.occupiedBy === "white", 
       mainField.isKing || mainField.occupiedBy === "white", 
-      mainField.isKing || mainField.occupiedBy === "dark", 
-      mainField.isKing || mainField.occupiedBy === "dark"
+      mainField.isKing || mainField.occupiedBy === "black", 
+      mainField.isKing || mainField.occupiedBy === "black"
     ] // [tl, tr, bl, br]
 
     const setFieldActive = (row: number, col: number): undefined => {
@@ -197,11 +227,6 @@ export default function Board() {
         fields_[nRow+i+1][nCol+i+1] && 
         fields_[nRow+i+1][nCol+i+1].occupiedBy !== "none"
       ) {br = undefined; isDirectionEnabled[3] = false}
-
-      // if (tl && tl.occupiedBy !== fields_[nRow][nCol].occupiedBy && checkExistanceAndNone(-i-1, -i-1)) tl = setFieldActive(nRow - i-1, nCol - i-1)
-      // if (tr && tr.occupiedBy !== fields_[nRow][nCol].occupiedBy && checkExistanceAndNone(-i-1, i+1)) tr = setFieldActive(nRow - i-1, nCol + i+1)
-      // if (bl && bl.occupiedBy !== fields_[nRow][nCol].occupiedBy && checkExistanceAndNone(i+1, -i-1)) bl = setFieldActive(nRow + i+1, nCol - i-1)
-      // if (br && br.occupiedBy !== fields_[nRow][nCol].occupiedBy && checkExistanceAndNone(i+1, i+1)) br = setFieldActive(nRow + i+1, nCol + i+1)
     }
     return fields_
   }
@@ -220,15 +245,15 @@ export default function Board() {
                 <div 
                   onClick={() => handlePieceClick2(nRow, nCol)} 
                   key={nRow + nCol} 
-                  className={`flex justify-center items-center relative ${field.color}`}
+                  className={`flex justify-center items-center relative ${(nRow + nCol) % 2 === 1 ? "bg-zinc-100" : "bg-zinc-900"}`}
                 >
                   {field.active ?
                     <FieldActive>
-                      {field.occupiedBy !== "none" && (field.occupiedBy === "dark" ? <Piece color="dark" isKing={field.isKing}/> : <Piece isKing={field.isKing}/>) }
+                      {field.occupiedBy !== "none" && (field.occupiedBy === "black" ? <Piece color="dark" isKing={field.isKing}/> : <Piece isKing={field.isKing}/>) }
                     </FieldActive>
                     : 
                     <>
-                      {field.occupiedBy !== "none" && (field.occupiedBy === "dark" ? <Piece color="dark" isKing={field.isKing}/> : <Piece isKing={field.isKing} />) }
+                      {field.occupiedBy !== "none" && (field.occupiedBy === "black" ? <Piece color="dark" isKing={field.isKing}/> : <Piece isKing={field.isKing} />) }
                     </>
                   }
                 </div>
@@ -252,7 +277,7 @@ export default function Board() {
     <br/>
     <div className="flex flex-col items-start justify-center gap-5">
     <button className="bg-rose-600 rounded-md px-4 py-2" onClick={() => setFields(generateFields())}>Restart stuff</button>
-    <button className="bg-rose-600 rounded-md px-4 py-2" onClick={() => setWhosTurn(whosTurn === "dark" ? "white" : "dark")}>Toggle turn</button>
+    <button className="bg-rose-600 rounded-md px-4 py-2" onClick={() => setWhosTurn(whosTurn === "black" ? "white" : "black")}>Toggle turn</button>
     </div>
     </>
   )
